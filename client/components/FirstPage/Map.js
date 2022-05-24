a84abeefa65b6a5bf25a1df656ae253f4ef75bd7
bengreenspan <91162661+bengreenspan@users.noreply.github.com>
2022-05-23 21:40:18 -0400

    map

--- client/components/FirstPage/Map.js
+++ client/components/FirstPage/Map.js
@@ -42,7 +42,7 @@ const Map = () => {
           }}
         >
           <BS.Row>
-            <BS.Col md={7} sm={12} xs={12}>
+            <BS.Col md={7} sm={7} xs={12}>
               <div
                 className="formpage"
                 // data-aos="zoom-in"
@@ -114,7 +114,7 @@ const Map = () => {
                 </section>
               </div>
             </BS.Col>
-            <BS.Col md={7} sm={12} xs={12}>
+            <BS.Col md={5} sm={5} xs={12}>
               <div
                 // data-aos="fade-left"
                 // data-aos-duration="1000"
